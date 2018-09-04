19dd253028c71ae07f31d91d50c9e31169c473fb
zhaoxu <zhaoxu841020@163.com>
2018-09-04 23:14:43 +0800

    加入springboot-admin

---
+++ spring-cloud-admin/src/main/java/com/dixn/spring/cloud/admin/SpringCloudAdminApplication.java
@@ -0,0 +1,16 @@
+package com.dixn.spring.cloud.admin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableAdminServer
+@EnableEurekaClient
+public class SpringCloudAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudAdminApplication.class, args);
+    }
+}
---
+++ spring-cloud-admin/src/test/java/com/dixn/spring/cloud/admin/SpringCloudAdminApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dixn.spring.cloud.admin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCloudAdminApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
